b96a940677dc00990d7eabd40501014bb89910e3
Leon180 <lioung860302@gmail.com>
2023-02-17 22:24:37 +0800

    feat: add models of user and espense

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const useExphbs = require('./config/hbs')
 const routes = require('./routers/index')
+// .env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const db = require('./config/mongoose')
+
+
+
 const app = express()
 const port = 3000
+
 
 useExphbs(app)
 app.use(express.static('public'))
